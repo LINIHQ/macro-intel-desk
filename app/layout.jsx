9bac4db703b750ff879d01e9e360fee83ea2f1b3
LINIHQ <dev@linihq.com>
2026-08-17 20:15:50 -0400

    Set home screen app title to XRP Brief

--- app/layout.jsx
+++ app/layout.jsx
@@ -28,7 +28,7 @@ export const metadata = {
   appleWebApp: {
     capable: true,
     statusBarStyle: 'black-translucent',
-    title: 'GenXKrypto',
+    title: 'XRP Brief',
   },
   openGraph: {
     title: TITLE,
